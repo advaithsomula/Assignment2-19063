d9caff30606589f1a9ad7a4f029ea4948fbbf763
advaithsomula <64926134+advaithsomula@users.noreply.github.com>
2022-10-08 06:23:05 +0530

    Add files via upload

---
+++ smart_journal-master/journal/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path 
+from . import views 
+from . views import JournalList 
+
+urlpatterns = [
+    path('',views.create_journal,name='home'),
+    path('create',views.create_journal, name="create_journal"),
+    path('listjournals', views.listjournals, name="listjournals"),
+    path('search', views.search, name="search"),
+    path('listall',JournalList.as_view(), name="listall"),
+    path('delete/<int:id>/',views.delete, name="delete"),
+    path('update/<int:id>/',views.update, name="update"),
+    path('journal_detail/<int:id>',views.journal_detail, name="journal_detail"),
+    path('index.html',views.wordle,name='wordle')
+]
